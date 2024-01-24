cddf564649679e30ba12d654aeabcbe95b77f728
itsayush30 <itsayush30@gmail.com>
2024-01-24 23:17:41 +0530

    Added express server and docker

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app = express();
+
+app.get('/home', (req, res) => {
+    res.json({message: 'OK'});
+});
+
+app.listen(process.env.PORT, () => {
+    console.log("started the server");
+});
